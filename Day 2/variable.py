d8f4baa596a5079af883a66d43edbeb32a2ad2da
Bill Eger <egeriicw@gmail.com>
2023-09-22 19:12:13 -0400

    New repository cleanup

---
+++ Day 2/variable.py
@@ -0,0 +1,60 @@
+# Day 2: 30 Days of Programming
+
+# Exercise Level 1
+
+first_name = 'Bill'
+last_name = 'Eger'
+full_name = 'Bill Eger'
+country = 'United States of America'
+city = 'Washington, D.C.'
+age = 44
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+is_house_open, is_door_locked, square_feet = False, True, 2500
+
+
+# Exercise Level 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(is_house_open))
+print(type(is_door_locked))
+print(type(square_feet))
+
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one**num_two
+floor_division = num_one // num_two
+
+
+radius = 30 # 30 meters
+area_of_circle = 3.14 * radius**2
+circum_of_circle = 2 * 3.14 * radius
+input_radius = int(input('radius: '))
+input_area_of_circle = 3.14 * input_radius**2
+
+input_first_name = input('First Name: ')
+input_last_name = input('Last Name: ')
+input_country = input('Country: ')
+input_age = input('Age: ')
+
+help('keywords')
